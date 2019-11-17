964edafd780d212d4dec2e24c28b7fb13bbbd2eb
Adamos45 <37145552+Adamos45@users.noreply.github.com>
2019-11-18 00:51:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ def main():
     prepare_data(files)
     data = read_data(files)
     train_models(data, files)
-
+    
+    model = svm.SVC(C=0.8, kernel='rbf', degree=4, gamma='auto',
+                        coef0=0.0, shrinking=True, probability=False, tol=0.001,
+                        cache_size=200, class_weight='balanced', verbose=False,
+                        max_iter=-1, decision_function_shape='ovo', random_state=1)
+     
+    model = DecisionTreeClassifier(class_weight='balanced')
 
 if __name__ == "__main__":
     main()
